940672782eecf1d24397092e0461094c1c83b540
Konrad Ryłko <konrad.rylko@gmail.com>
2016-01-23 12:28:07 +0100

    Update the main class

--- lib/inpost_client.rb
+++ lib/inpost_client.rb
@@ -7,20 +7,21 @@ class InpostClient
   attr_reader :endpoint_url, :machines, :machine
 
   def initialize url=nil
-    @endpoint_url = url || inpost_url
-    @machines = InpostClient::MachinesLoader.new(endpoint_url)
-
+    @endpoint_url = url || "https://api-pl.easypack24.net/v4"
+    @machines = machine_loader @endpoint_url
   end
 
-  def machines_by_type type
-    @machines.by_type type
+  def load_machines
+    @machines = machine_loader @endpoint_url
   end
 
   def machine id
-    @machines.machine id
+    @machines.find id
   end
 
-  def inpost_url
-    "https://api-pl.easypack24.net/v4"
+  private
+
+  def machine_loader url
+    InpostClient::MachinesLoader.new url
   end
 end
--- spec/inpost_client_spec.rb
+++ spec/inpost_client_spec.rb
@@ -7,18 +7,27 @@ describe InpostClient do
   let(:machines_loader) { described_class::MachinesLoader.new(url) }
   let(:machines_loader_class) { machines_loader_class }
 
-  it "has a version number" do
-    expect(InpostClient::VERSION).not_to be nil
+  before :each do
+    allow_any_instance_of(described_class::MachinesLoader).to receive(:get_machines_json).and_return machines_json
   end
 
   it "has defined the endpoint url" do
-    allow_any_instance_of(described_class::MachinesLoader).to receive(:get_machines).and_return machines_json
     expect(subject.endpoint_url).to eq url
     expect(described_class.new.endpoint_url).to eq "https://api-pl.easypack24.net/v4"
   end
 
   it "gets machines" do
-    allow_any_instance_of(described_class::MachinesLoader).to receive(:get_machines).and_return machines_json
     expect(subject.machines).to be_an described_class::MachinesLoader
+  end
+
+  it "loads machines" do
+    expect(described_class::MachinesLoader).to receive(:new).twice
+    # twice because of before each filter
+    subject.load_machines
+  end
+
+  it "get machine by id" do
+    expect(subject.machines).to receive(:find).with('abc')
+    subject.machine "abc"
   end
 end
