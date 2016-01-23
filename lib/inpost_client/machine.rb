8fc0cfcc9dac7e86d535262cbff9504d898cc8b1
Konrad Ryłko <konrad.rylko@gmail.com>
2016-01-23 23:50:11 +0100

    Fix names in machine class

--- lib/inpost_client/machine.rb
+++ lib/inpost_client/machine.rb
@@ -7,11 +7,11 @@ class InpostClient
     private
 
     def create_attributes machine_raw
-      method_names = machine_raw.keys.find_all{ |x| x.to_s[0] != '_' }
+      attribute_names = machine_raw.keys.find_all{ |x| x.to_s[0] != '_' }
 
-      method_names.each do |method_name|
-        instance_variable_set "@#{method_name}", machine_raw[method_name]
-         InpostClient::Machine.class_eval{attr_reader method_name}
+      attribute_names.each do |attribute_name|
+        instance_variable_set "@#{attribute_name}", machine_raw[attribute_name]
+         InpostClient::Machine.class_eval{attr_reader attribute_name}
       end
     end
   end
