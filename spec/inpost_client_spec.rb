923c776c48fe4a2324ea4d3463cbd7421a1d6938
Konrad Ryłko <konrad.rylko@gmail.com>
2016-01-24 21:41:54 +0100

    Fix specs

--- spec/inpost_client_spec.rb
+++ spec/inpost_client_spec.rb
@@ -1,29 +1,22 @@
 require "spec_helper"
 
 describe InpostClient do
-  subject { described_class.new url }
+  subject { described_class.new nil, url }
   let(:url) { "http://endpoint_url.com" }
   let(:machines_json) { InpostClient::Specs.load_machines_json }
-  let(:machines_loader) { described_class::MachinesLoader.new(url) }
-  let(:machines_loader_class) { machines_loader_class }
+  let(:machines_loader_class) { described_class::MachinesLoader }
 
   before :each do
-    allow_any_instance_of(described_class::MachinesLoader).to receive(:get_machines_json).and_return machines_json
+    allow_any_instance_of(machines_loader_class).to receive(:get_machines_json).and_return machines_json
   end
 
-  it "has defined the endpoint url" do
-    expect(subject.endpoint_url).to eq url
-    expect(described_class.new.endpoint_url).to eq "https://api-pl.easypack24.net/v4"
+  it "has defined the Inpost url" do
+    expect(subject.inpost_url).to eq url
+    expect(described_class.new.inpost_url).to eq "https://api-pl.easypack24.net/v4"
   end
 
   it "gets machines" do
-    expect(subject.machines).to be_an described_class::MachinesLoader
-  end
-
-  it "loads machines" do
-    expect(described_class::MachinesLoader).to receive(:new).twice
-    # twice because of before each filter
-    subject.load_machines
+    expect(subject.machines).to be_an machines_loader_class
   end
 
   it "get machine by id" do
